199099513ac23862ca3801f518654c755f832c47
Ashwith Thatipally <ashwiththatipally@gmail.com>
2026-09-09 14:48:24 +0530

    Time: 4 ms (9.38%) | Memory: 63.2 MB (5.63%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -3,9 +3,11 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    let n = Math.floor(s.length/2)
-    let n1 = s.length-1
-    for(let i=0;i<n;i++){
-        [s[i],s[n1-i]]=[s[n1-i],s[i]]
+    let i=0
+    let j = s.length-1
+    while(i<=j){
+        [s[i],s[j]]=[s[j],s[i]]
+        i++
+        j--
     }
 };
