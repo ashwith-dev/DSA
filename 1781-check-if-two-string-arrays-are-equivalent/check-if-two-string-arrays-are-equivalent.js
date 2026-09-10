814b44813a8d5468fb1340daf368e15cc144fa16
Ashwith Thatipally <ashwiththatipally@gmail.com>
2026-09-10 17:11:39 +0530

    Time: 0 ms (100.00%) | Memory: 54.9 MB (10.55%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.js
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.js
@@ -4,14 +4,7 @@
  * @return {boolean}
  */
 var arrayStringsAreEqual = function(word1, word2) {
-    let first = ""
-    let secound = ""
-    for(let i=0; i<word1.length; i++){
-        first+=word1[i]
-    }
-    for(let i=0; i<word2.length; i++){
-        secound+=word2[i]
-    }
-    if(first == secound ) return true
-    return false
+    let first = word1.join('')
+    let secound = word2.join('')
+    return first==secound
 };
