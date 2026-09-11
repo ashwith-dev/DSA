e4573f4c57d85779de40a05a4f8301c33feac014
Ashwith Thatipally <ashwiththatipally@gmail.com>
2026-09-11 11:13:07 +0530

    Time: 35 ms (25.81%) | Memory: 77 MB (91.71%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -5,8 +5,8 @@
 var containsDuplicate = function(nums) {
     let list = new Map()
     for(let i=0; i<nums.length; i++){
-        list.set(nums[i], (list.get(nums[i]) || 0) + 1)
-        if(list.get(nums[i]) > 1) return true 
+        list.set(nums[i],(list.get(nums[i]) || 0) + 1)
+        if(list.get(nums[i]) > 1) return true
     }
     return false
 };
