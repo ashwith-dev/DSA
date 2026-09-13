ec4584468d37ace90b16e0e0349ff8774f4f39d2
Ashwith Thatipally <ashwiththatipally@gmail.com>
2026-09-13 12:57:54 +0530

    Time: 0 ms (100.00%) | Memory: 54.4 MB (36.65%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -6,19 +6,6 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    // let j=0
-    // for(let i=0; i<nums1.length; i++){
-    //     if(nums1[i]==0){
-    //         nums1[i]=nums2[j]
-    //         j++
-    //     }
-    // }
-    // if(nums1.length!==m+n){
-    //     while(nums1.length<=m+n){
-    //         nums1.push(0)
-    //     }
-    // }
-    // nums1=nums1.sort((a,b)=>a-b)
     let j=0
     for(let i=m;i<nums1.length;i++){
         nums1[i]=nums2[j]
