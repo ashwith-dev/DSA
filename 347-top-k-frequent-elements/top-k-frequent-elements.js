839854670a9912169a09ee198529cffe7194664d
Ashwith Thatipally <ashwiththatipally@gmail.com>
2026-09-13 13:32:23 +0530

    Time: 33 ms (7.44%) | Memory: 63.7 MB (17.22%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    let list = new Map()
+    let arr = []
+    for(let i=0;i<nums.length;i++){
+        list.set(nums[i],(list.get(nums[i]) || 0) + 1)
+    }
+    while(arr.length<k){
+        let freq = 0
+        let key1 = 0
+        for(let [key,value] of list){
+            if(value>freq){
+                freq=value
+                key1 = key
+            }
+        }
+        if(!arr.includes(key1)){
+            arr.push(key1)
+            list.set(key1,0)
+        } 
+    }
+    return arr
+};
