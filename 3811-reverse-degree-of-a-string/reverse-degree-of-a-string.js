37323472b229466bcbcb1b346497c886a42b293d
Ashwith Thatipally <ashwiththatipally@gmail.com>
2026-09-21 14:30:04 +0530

    Time: 1 ms (80.95%) | Memory: 57.7 MB (14.29%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.js
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.js
@@ -6,14 +6,7 @@ var reverseDegree = function(s) {
     let sum = 0
     for(let i=0; i<s.length; i++){
         let current = s.charCodeAt(i)
-        console.log("current :",current)
-        if(current >= 'a'.charCodeAt(0) && current <= 'z'.charCodeAt(0)){
-            sum+=('z'.charCodeAt(0)-current+1) * (i+1);
-            console.log("1st loop :",sum)
-        }else{
-            sum+=('Z'.charCodeAt(0)-current+1) * (i+1);
-            console.log("2nd loop :",sum)
-        }
+        sum+=('z'.charCodeAt(0)-current+1) * (i+1);
     }
     return sum
 };
